b29ce009e417accdfbb0d6c0941d1e5b8ed9c6aa
David Boedigheimer <stealth259@gmail.com>
2021-01-13 15:41:33 -0800

    Add finished controllers for client and stylist.

--- HairSalon/Contollers/StylistsController.cs
+++ HairSalon/Contollers/StylistsController.cs
@@ -21,5 +21,25 @@ namespace HairSalon.Controllers
             List<Stylist> model = _db.Stylists.ToList();
             return View(model);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Stylist stylist)
+        {
+            _db.Stylists.Add(stylist);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Stylist thisStylist = _db.Stylists.Include(stylist => stylist.Clients)
+                .FirstOrDefault(stylist => stylist.StylistId == id);
+            return View(thisStylist);
+        }
     }
 }
